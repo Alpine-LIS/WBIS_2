6fb343783078beb4011700425fd0b62729648616
alpineTsuran <43612592+alpineTsuran@users.noreply.github.com>
2022-07-29 14:32:04 -0700

    a

---
+++ WBIS-2.Modules/Views/RecordImporters/SPINOGOImportView.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WBIS_2.Modules.ViewModels.RecordImporters;
+
+namespace WBIS_2.Modules.Views.RecordImporters
+{
+    /// <summary>
+    /// Interaction logic for SPINOGOImportView.xaml
+    /// </summary>
+    public partial class SPINOGOImportView : UserControl
+    {
+        public SPINOGOImportView()
+        {
+            InitializeComponent();
+            DataContext = new SPINOGOImportViewModel();
+        }
+    }
+}
